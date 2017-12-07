a6caa81b43745a88af7f4e42dd900a9c8860207b
Pedro Charun <mlosail3@gmail.com>
2017-12-07 12:58:20 -0500

    dev(BackEnd): Created a UsersSeeders.

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $faker = Faker::create();
+      DB::table('users')->insert([
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => bcrypt('secret'),
+        'remember_token' => str_random(10),
+      ]);
+    }
+}
